0dd9fdcee56d4750d49c43d956aa00a4afe7e22b
Aaron Wiebe <awiebe1@cox.net>
2021-07-20 16:03:49 -0700

    added error checking

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,7 +10,7 @@ if (cityArray){
     var searchedCities = [];
 }; 
 
-var Sector = ["N","NNE","NE","ENE","E","ESE","SE","SSE","S","SSW","SW","WSW","W","WNW","NW","NNW","N"];
+var Sector = ["N","NNE","NNE","NE","NE","ENE","ENE","E","E","ESE","ESE","SE","SE","SSE","SSE","S","S","SSW","SSW","SW","SW","WSW","WSW","W","W","WNW","WNW","NW","NW","NNW","NNW","N"];
 
 for (let i = 1; i < 6; i++) {
 document.querySelector("#Day"+i).textContent = moment().add(i, 'days').format('L');
@@ -19,6 +19,9 @@ document.querySelector("#Day"+i).textContent = moment().add(i, 'days').format('L
 loadCities();
 
 var searchCityAction = function(){
+  if(document.querySelector("#submit-search").textContent == "City Not Found"){
+    resetSubmitButton(); 
+  };
   var searchStr = document.querySelector("#searchTerm").value;
   if (searchStr){
     const arr = searchStr.split(" ");
@@ -31,36 +34,93 @@ var searchCityAction = function(){
       localStorage.setItem("searchedCities" , JSON.stringify(searchedCities));
     };
       
-// //save data as key,value: dayyear, array of tasks per hour
+
     var apiUrl = "https://api.geocod.io/v1.6/geocode?city=" + searchStr + "&api_key=3212807212bfb01b6636f32077439307032f440"
     fetch(apiUrl)
       .then(geoResponse => geoResponse.json())
+
       .then(geoResponse => {
         console.log(geoResponse.results[0])
         var divEl = document.querySelector("#response-container");
 
-        let currentCity = geoResponse.results[0].formatted_address;
+        let currentCity = geoResponse.results[0].address_components.city + ", " + geoResponse.results[0].address_components.state;
         var para = document.querySelector("#display-city"); 
-        para.textContent = currentCity; 
+        para.textContent = currentCity + " for " + moment().format('LL'); 
         var api2Url = "https://api.openweathermap.org/data/2.5/onecall?lat="+geoResponse.results[0].location.lat+"&lon="+geoResponse.results[0].location.lng+"&exclude=minutely,hourly&appid=c6372f1324c78c2e38ccaa1ebef5b15c"
         fetch(api2Url)
         .then(response => response.json())
         .then(response => {
           document.querySelector("#current-temp").textContent = "Temp: " + (((response.current.temp-273.15) * (9/5)) + 32).toFixed(2);
-          document.querySelector("#current-wind").textContent = "Wind: " + Sector[(Math.round(response.current.wind_deg / 22.5) + 1)] + " @ " + response.current.wind_speed + 'mph';
+          document.querySelector("#current-wind").textContent = "Wind: " + Sector[(Math.round(response.current.wind_deg / 11.25))] + " @ " + response.current.wind_speed + 'mph';
           document.querySelector("#current-hum").textContent = "Humidity: " + response.current.humidity;
           document.querySelector("#current-UVI").textContent = "UV Index: " + response.current.uvi;
+          switch(Math.floor(response.current.uvi)) {
+            case 1:
+            case 2:
+              document.querySelector("#current-UVI").style.backgroundColor = "green"
+              document.querySelector("#current-UVI").style.color = "black";
+            break;
+            case 3:
+            case 4:
+            case 5:
+              document.querySelector("#current-UVI").style.backgroundColor = "yellow"
+              document.querySelector("#current-UVI").style.color = "black";
+            break;
+            case 6:
+            case 7:
+              document.querySelector("#current-UVI").style.backgroundColor = "orange"
+              document.querySelector("#current-UVI").style.color = "black";
+            break;
+            case 8:
+            case 9:
+            case 10:
+              document.querySelector("#current-UVI").style.backgroundColor = "red"
+              document.querySelector("#current-UVI").style.color = "white";
+            break;
+            case 11:
+              document.querySelector("#current-UVI").style.backgroundColor = "purple"
+              document.querySelector("#current-UVI").style.color = "white";
+            break;
+            default:
+              document.querySelector("#current-UVI").style.backgroundColor = "purple"
+              document.querySelector("#current-UVI").style.color = "white";
+          } 
+          
+
+          document.querySelector("#current-icon").src="http://openweathermap.org/img/wn/" + response.daily[i].weather[0].icon + "@2x.png";
+          document.querySelector("#current-wind-bearing").src="./assets/images/arrow-icon-transparent-background-24.jpg";
+          document.querySelector("#current-wind-bearing").style.transform = "rotate(" + response.current.wind_deg + "deg)";
+
 
           for (let i = 1; i < 6; i++) {
-            document.querySelector("#Day"+i+"-temp").textContent = "Temp: " + (((response.daily[i].temp.max-273.15) * (9/5)) + 32).toFixed(2);
-            document.querySelector("#Day"+i+"-wind").textContent = "Wind: " + Sector[(Math.round(response.daily[i].wind_deg / 22.5) + 1)] + "@" + response.daily[i].wind_speed.toFixed(0);
+            document.querySelector("#Day"+i+"-temp").textContent = "Hi Temp: " + (((response.daily[i].temp.max-273.15) * (9/5)) + 32).toFixed(2);
+            document.querySelector("#Day"+i+"-wind").textContent = "Wind: " + Sector[(Math.round(response.daily[i].wind_deg / 11.25))] + "@" + response.daily[i].wind_speed.toFixed(0);
             document.querySelector("#Day"+i+"-hum").textContent = "Humidity: " + response.daily[i].humidity;
             document.querySelector("#Day"+i+"-icon").src="http://openweathermap.org/img/wn/" + response.daily[i].weather[0].icon + "@2x.png";
           };
         }); 
+      })
+      .catch(function() {
+        searchStr = '';
+        document.querySelector("#searchTerm").value = "";
+        searchedCities = JSON.parse(localStorage.getItem("searchedCities"));
+        let popped = searchedCities.pop();
+        localStorage.setItem("searchedCities" , JSON.stringify(searchedCities));
+        console.log("error");
+        document.querySelector("#submit-search").textContent = "City Not Found";
+        document.querySelector("#submit-search").style.backgroundColor = "red";
+        loadCities();   
       });
     loadCities();
-  };    
+  };
+  // if(document.querySelector("#submit-search").textContent == "City Not Found"){
+  //   window.setTimeout(resetSubmitButton(), 8000); 
+  // };
+};
+
+function resetSubmitButton(){
+  document.querySelector("#submit-search").textContent = "Submit";
+  document.querySelector("#submit-search").style.backgroundColor = "#06AED5"; 
 };
 
 function searchPastCity(){
@@ -82,6 +142,7 @@ function loadCities(){
      button.className = "btn";
      button.innerHTML = cityText;
      button.addEventListener('click', searchPastCity, false);
+    //  button.onclick = "searchPastCity()";
      listItemEl.appendChild(button);
      document.getElementById("city-list").appendChild(listItemEl);
    };
