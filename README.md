d3c3a7e946eb0b25e7ee0af6e5a2ee4e03e6fb67
Aaron Wiebe <awiebe1@cox.net>
2021-07-23 07:48:25 -0700

    commented code and readme

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # Weather-Dashboard
+
+
+## Description 
+
+Server-Side APIs Challenge: Weather Dashboard
+Challenge Elements
+Third-party APIs allow developers to access their data and functionality by making requests 
+with specific parameters to a URL. Developers are often tasked with retrieving data from 
+another application's API and using it in the context of their own. Your challenge is to 
+build a weather dashboard that will run in the browser and feature dynamically updated HTML and CSS.
+
+
+## Installation
+
+The project can be viewed at: https://AWiebe2021.github.io/Weather-Dashboard/
+The projects code can be accessed at: https://github.com/AWiebe2021/Weather-Dashboard/
+
+## Screenshot
+![Weather-Dashboard Screenshot](./assets/images/Screenshot.png)
+
+## Credits
+Solo project this sixth week
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,10 @@
+//global vars
 var day = (moment().format("DDDDYYYY"));
 var dayInc = 0;
 var hour = moment().hours();
-// var searchedCities = [];
+var Sector = ["N","NNE","NNE","NE","NE","ENE","ENE","E","E","ESE","ESE","SE","SE","SSE","SSE","S","S","SSW","SSW","SW","SW","WSW","WSW","W","W","WNW","WNW","NW","NW","NNW","NNW","N"];
 
+//if cities exist in storage set array or setup blank array
 var cityArray = JSON.parse(localStorage.getItem("searchedCities"));
 if (cityArray){
     searchedCities = cityArray;
@@ -10,19 +12,22 @@ if (cityArray){
     var searchedCities = [];
 }; 
 
-var Sector = ["N","NNE","NNE","NE","NE","ENE","ENE","E","E","ESE","ESE","SE","SE","SSE","SSE","S","S","SSW","SSW","SW","SW","WSW","WSW","W","W","WNW","WNW","NW","NW","NNW","NNW","N"];
-
+//Setup 5 Day cards
 for (let i = 1; i < 6; i++) {
 document.querySelector("#Day"+i).textContent = moment().add(i, 'days').format('L');
 };
 
+//Setup city buttons
 loadCities();
 
+//Main function
 var searchCityAction = function(){
+  //reset error catch
   if(document.querySelector("#submit-search").textContent == "City Not Found"){
     resetSubmitButton(); 
   };
   var searchStr = document.querySelector("#searchTerm").value;
+  //capitalize each word before storing it
   if (searchStr){
     const arr = searchStr.split(" ");
     for (var i = 0; i < arr.length; i++) {
@@ -34,27 +39,30 @@ var searchCityAction = function(){
       localStorage.setItem("searchedCities" , JSON.stringify(searchedCities));
     };
       
-
+    //first fetch
     var apiUrl = "https://api.geocod.io/v1.6/geocode?city=" + searchStr + "&api_key=3212807212bfb01b6636f32077439307032f440"
     fetch(apiUrl)
       .then(geoResponse => geoResponse.json())
-
       .then(geoResponse => {
         console.log(geoResponse.results[0])
-        var divEl = document.querySelector("#response-container");
+        // var divEl = document.querySelector("#response-container");
 
         let currentCity = geoResponse.results[0].address_components.city + ", " + geoResponse.results[0].address_components.state;
         var para = document.querySelector("#display-city"); 
         para.textContent = currentCity + " for " + moment().format('LL'); 
+        //second fetch (nested) 
         var api2Url = "https://api.openweathermap.org/data/2.5/onecall?lat="+geoResponse.results[0].location.lat+"&lon="+geoResponse.results[0].location.lng+"&exclude=minutely,hourly&appid=c6372f1324c78c2e38ccaa1ebef5b15c"
         fetch(api2Url)
         .then(response => response.json())
         .then(response => {
+          // current conditions - temp connverted from Kelvin, wind converted to sector
           document.querySelector("#current-temp").textContent = "Temp: " + (((response.current.temp-273.15) * (9/5)) + 32).toFixed(2);
           document.querySelector("#current-wind").textContent = "Wind: " + Sector[(Math.round(response.current.wind_deg / 11.25))] + " @ " + response.current.wind_speed + 'mph';
           document.querySelector("#current-hum").textContent = "Humidity: " + response.current.humidity;
           document.querySelector("#current-UVI").textContent = "UV Index: " + response.current.uvi;
+          //UV Index color coding per the EPA (low, moderate, high, very high, extreme)
           switch(Math.floor(response.current.uvi)) {
+            case 0:
             case 1:
             case 2:
               document.querySelector("#current-UVI").style.backgroundColor = "green"
@@ -86,12 +94,12 @@ var searchCityAction = function(){
               document.querySelector("#current-UVI").style.color = "white";
           } 
           
-
+          //setting current weather icon and wind bearing
           document.querySelector("#current-icon").src="http://openweathermap.org/img/wn/" + response.daily[i].weather[0].icon + "@2x.png";
           document.querySelector("#current-wind-bearing").src="./assets/images/arrow-icon-transparent-background-24.jpg";
           document.querySelector("#current-wind-bearing").style.transform = "rotate(" + response.current.wind_deg + "deg)";
 
-
+          //setting 5 day forcast
           for (let i = 1; i < 6; i++) {
             document.querySelector("#Day"+i+"-temp").textContent = "Hi Temp: " + (((response.daily[i].temp.max-273.15) * (9/5)) + 32).toFixed(2);
             document.querySelector("#Day"+i+"-wind").textContent = "Wind: " + Sector[(Math.round(response.daily[i].wind_deg / 11.25))] + "@" + response.daily[i].wind_speed.toFixed(0);
@@ -100,6 +108,7 @@ var searchCityAction = function(){
           };
         }); 
       })
+      //Error catch and warning
       .catch(function() {
         searchStr = '';
         document.querySelector("#searchTerm").value = "";
@@ -109,13 +118,11 @@ var searchCityAction = function(){
         console.log("error");
         document.querySelector("#submit-search").textContent = "City Not Found";
         document.querySelector("#submit-search").style.backgroundColor = "red";
-        loadCities();   
+        loadCities();
+        window.setTimeout(resetSubmitButton, 3000); 
       });
     loadCities();
   };
-  // if(document.querySelector("#submit-search").textContent == "City Not Found"){
-  //   window.setTimeout(resetSubmitButton(), 8000); 
-  // };
 };
 
 function resetSubmitButton(){
@@ -130,10 +137,12 @@ function searchPastCity(){
 
 function loadCities(){
   var searchedCities = JSON.parse(localStorage.getItem("searchedCities"));
-  var answersEl = document.getElementById("city-list");
-  while (answersEl.hasChildNodes()) {  
-    answersEl.removeChild(answersEl.firstChild);
+  //erase existing buttons
+  var citiesEl = document.getElementById("city-list");
+  while (citiesEl.hasChildNodes()) {  
+    citiesEl.removeChild(citiesEl.firstChild);
   };
+  //Setup a button for each city
   if (searchedCities){
    for (let i = 0; i < searchedCities.length; i++) {
      var cityText = searchedCities[i];
@@ -142,7 +151,6 @@ function loadCities(){
      button.className = "btn";
      button.innerHTML = cityText;
      button.addEventListener('click', searchPastCity, false);
-    //  button.onclick = "searchPastCity()";
      listItemEl.appendChild(button);
      document.getElementById("city-list").appendChild(listItemEl);
    };
